11cb104c1fb3d49f56cf0bf9856ae79d98638c81
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-04-28 15:14:43 +0600

    design completion

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,21 +3,32 @@ import React, { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets/assets";
 
 const Demo = () => {
+
+  const [article, setArticle] = useState({
+    url: "",
+    summary: '',
+  })
+
+  const handleSubmit = async (e) => {
+    alert('submitted')
+    e.preventDefault()
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* search section  */}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => { }}>
+          onSubmit={handleSubmit}>
           <img src={linkIcon} alt="link" className="absolute left-0 my-2 ml-3 w-5" />
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => { }}
+            value={article.url}
+            onChange={(e) => setArticle({article, url: e.target.value})}
             required
-            className="url_input peer"
+            className="url_input peer"  // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
           <button
             type='submit'
@@ -26,7 +37,11 @@ const Demo = () => {
             <p>↵</p>
           </button>
         </form>
+
+        {/* Browse URL History */}
+
       </div>
+      {/* Display result */}
     </section>
   )
 }
