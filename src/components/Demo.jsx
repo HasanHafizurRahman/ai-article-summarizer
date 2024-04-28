ab5ecefcfc6c9aa8021e60d54359e9056fa14d72
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-04-28 13:03:08 +0600

    input field design

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets/assets";
 
 const Demo = () => {
   return (
-    <div>
-        Demo
-    </div>
+    <section className="mt-16 w-full max-w-xl">
+      {/* search section  */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => { }}>
+          <img src={linkIcon} alt="link" className="absolute left-0 my-2 ml-3 w-5" />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => { }}
+            required
+            className="url_input peer"
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <p>↵</p>
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
